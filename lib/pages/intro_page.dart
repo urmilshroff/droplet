eee3dc2f4341a1e4f0913f3c1dd6c5b0701251c2
urmilshroff <urmilshroff@gmail.com>
2020-02-21 14:12:04 +0530

    Added stuff from Dashboard Reborn

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,112 @@
+import 'package:droplet/pages/home_page.dart';
+import 'package:droplet/utils/colors.dart';
+import 'package:droplet/utils/text_styles.dart';
+import 'package:droplet/utils/ui_helpers.dart';
+import 'package:droplet/widgets/sexy_tile.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyIntroPage extends StatefulWidget {
+  @override
+  _MyIntroPageState createState() => _MyIntroPageState();
+}
+
+class _MyIntroPageState extends State<MyIntroPage> {
+  List<String> itemContent = [
+    'Welcome to Droplet!',
+  ]; //the text in the tile
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: invertInvertColorsStrong(context),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 60.0,
+                bottom: 10.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Hey there!',
+                    style: isThemeCurrentlyDark(context)
+                        ? TitleStylesDefault.white
+                        : TitleStylesDefault.black,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  Hero(
+                    tag: 'tile2',
+                    child: SexyTile(
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              itemContent[0],
+                              style: HeadingStylesDefault.accent,
+                              textAlign: TextAlign.center,
+                              softWrap: true,
+                              overflow: TextOverflow.fade,
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              itemContent[1],
+                              style: isThemeCurrentlyDark(context)
+                                  ? BodyStylesDefault.white
+                                  : BodyStylesDefault.black,
+                              textAlign: TextAlign.left,
+                              softWrap: true,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ],
+                        ),
+                      ),
+                      splashColor: MyColors.accent,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        heroTag: 'fab',
+        child: Icon(
+          EvaIcons.checkmark,
+          size: 30.0,
+        ),
+        tooltip: 'Continue',
+        foregroundColor: invertInvertColorsStrong(context),
+        backgroundColor: invertInvertColorsTheme(context),
+        elevation: 5.0,
+        onPressed: () {
+          Navigator.push(
+            context,
+            CupertinoPageRoute(
+              builder: (context) {
+                return MyHomePage();
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
